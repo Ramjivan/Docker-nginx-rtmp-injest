f1b19a99c33f0a10b9cc1addc2c3f3c0df92b62c
Ramjivan Jangid <ramjeewanj@gmail.com>
2021-11-09 10:29:56 +0530

    removed default api Host url

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ RTMP_CONNECTIONS=${RTMP_CONNECTIONS-1024}
 RTMP_STREAM_NAMES=${RTMP_STREAM_NAMES-live,testing}
 RTMP_STREAMS=$(echo ${RTMP_STREAM_NAMES} | sed "s/,/\n/g")
 RTMP_PUSH_URLS=$(echo ${RTMP_PUSH_URLS} | sed "s/,/\n/g")
-API_HOST=${INJEST_API_HOST-'http://host.docker.internal:8080'}
+API_HOST=${INJEST_API_HOST}
 
 apply_config() {
 
